5b70abb7468d521775bfae35d6d901b314c211cb
Wes Jones <wjones127@gmail.com>
2022-10-05 23:56:09 -0400

    Initial commit

---
+++ test/tacticam.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as Tacticam from '../lib/tacticam-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/tacticam-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new Tacticam.TacticamStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
